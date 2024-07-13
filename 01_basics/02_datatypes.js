3ab9e3d0886a2ee64ac8ef51e408363ceb1ae02f
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-07-13 20:25:53 +0000

    done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("jay")
+
+
+let name = "jay"  //string data type
+let age = 20  //number
+let isLoggedIn = false  //boolean
+let state;  //undefined 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value // it is a representation of null value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
