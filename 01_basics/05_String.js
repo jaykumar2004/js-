a4e3f7de950a571a0004e3671a0f47ef8d0f3753
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-07-16 20:28:12 +0000

    string done

--- 01_basics/05_String.js
+++ 01_basics/05_String.js
@@ -5,7 +5,7 @@ console.log(name + repoCount + "values");
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  //new syntax to write string, we use back ticks(``)
 
 //another way to declare the string
-const gameName = new String('Jay Kumar')
+const gameName = new String('Jay-Kumar')
 
 
 
@@ -23,3 +23,25 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 
 console.log(gameName.indexOf('y'));
+
+const newString = gameName.substring(0, 4); //negative values do not accept here
+console.log(newString);
+
+const anotherString =  gameName.slice(-8, 4) //in this we can also give negative values, which starts from zero
+
+console.log(anotherString);
+
+//trim and replace method
+const newStringOne = "   jay kumar   ";
+console.log(newStringOne);        // without trim method  
+console.log(newString.trim());    //trim method => this method removes the space form the string
+//trim only works on white space character and line terminators
+
+//*******replace********** */
+const url = "http://jay.com/jay%20kumar" //apna browser spaces nhi samaghata he so we use replce method
+
+console.log(url.replace('%20', '-')); //"http://jay.com/jay-kumar" =>gives this output
+
+//we can also asks
+console.log(url.includes('jay'));  //it give true
+console.log(gameName.split('-')); //split the string on the bases of dash
