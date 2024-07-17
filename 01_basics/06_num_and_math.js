82789d65fdf3a6ee310e010634c4ac35e4850e4f
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-07-17 09:16:12 +0000

    Math and num

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -14,3 +14,31 @@ console.log(otherNumber.toPrecision(3));
 //if the number is 124.987 so the precision value comes 125
 const otherOneNumber = 124.987;
 console.log(otherOneNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-In')); // output is 1,000,000
+//when we add (en-In) it gives the indian format
+
+//+++++++++++++++++++++maths++++++++++++++++++++++++//
+
+console.log(Math); //Object [Math] {}
+console.log(Math.abs(-4)); // 4  - neg chang positive but + dont change
+console.log(Math.round(4.3)); //4 round of
+console.log(Math.ceil(4.2)); //choose top number , give output 5
+console.log(Math.floor(4.2)); //choose lower value, gives output 4
+
+console.log(Math.min(1,2,3,4)); //give minimum value from array
+console.log(Math.max(1,2,3,4)); //gives maxmimum value from array
+
+console.log(Math.random()); //give value between 0 and 1
+console.log(Math.random()*10); // shift values between 0 and 10 
+console.log((Math.random()*10) + 1); // to avoid zero we use +1
+//we can also use =>console.log(Math.floor(Math.random()*10) + 1);
+//to get only a number not in deciaml form
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random() * (max - min + 1))+ min); //only gets integer values 
+//max-min => for range
+// +1 => 0 case avoid
+//to get the minimum we use + min
