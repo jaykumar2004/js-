af806d579a2f9f3546d77bf0c7ea545b8b6a729f
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-08-31 20:06:34 +0530

    Global and local scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,4 +1,17 @@
 //yea khani start hoti he let,var and const se
-let a = 10;
-var b = 20;
-const c = 30;
+// var c = 300;
+
+let a=300;
+if(true){
+    let a = 10;
+    const b = 20;
+    console.log("Inner: ", a);
+    
+    // var c = 30;
+}  //this is a block scope and outside this loop is global scope
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
