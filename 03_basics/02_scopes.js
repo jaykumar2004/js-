05ae063e4c0c602f79bf3159ff3d93bc1d0b4c1e
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-09-07 15:41:31 +0530

    remaining

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,13 +5,39 @@ let a=300;
 if(true){
     let a = 10;
     const b = 20;
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
     
     // var c = 30;
 }  //this is a block scope and outside this loop is global scope
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+//nested scopes:
+function one(){
+    const username = "Jay kumar";
+
+    function two(){
+        const website = "youtube"; //error in this because "yea website ko keha access ker rehe ho, yea website to andar tha, iska scope to andar he khartam ho geya"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two();
+}
+one();
+
+
+if (true) {
+    const username = "jay";
+
+    if (username === "Jay") {
+        const website = " youtube";
+        console.log(username+website);
+    }
+    // console.log(website); //error in this 
+    
+}
+// console.log(username); //error in this code
 
