a8ac62e55021f188258c446d3b10ecbc1b2cdb78
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-09-10 15:25:23 +0530

    switch case in JS

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,52 @@
+//basic syntax:
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april";
+switch (month) {
+    case 1:
+        console.log("January"); //shift+alt+down arrow to make duplicate
+        break;
+    case 2:
+        console.log("Feburary");  
+        break;
+    case 3:
+        console.log("March");  
+        break;
+    case 4:
+        console.log("April");  
+        break;
+
+    default:
+        console.log("default case  match");
+        
+        break;
+}
+
+// //for string:
+// switch (month) {
+//     case "January":
+//         console.log("January"); //shift+alt+down arrow to make duplicate
+//         break;
+//     case "Feburary":
+//         console.log("Feburary");  
+//         break;
+//     case "March":
+//         console.log("March");  
+//         break;
+//     case "April":
+//         console.log("April");  
+//         break;
+
+//     default:
+//         console.log("default case  match");
+        
+//         break;
+// }
+
