b21c7aa15e461d0696a0aaca27f15e53011ed04b
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-09-10 14:40:40 +0530

    if-else basics and control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -2,9 +2,58 @@
 
 // if(true){   //in (), we want to write the condition
 // }
+// if(2 != 3){
+//     console.log("executed");
+    
+// }
+
+
+//if-else
+// const isUserLoggedIn = true;
+// const temerature = 41;
+// if(temerature === 41){
+//     console.log("less than 50");
+// }
+// else{
+//     console.log("greater than 50");
+// }
+// console.log("execute");
+
+
+
+// const score = 200;
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power ${power}`);
+// }
+// console.log(`user power ${power}`); //power is not defined
+
+
+// const balance = 1000
+// if(balance > 500) console.log("test");
+
+// if(balance < 500){
+//     console.log("less than 500")
+// }
+// else if(balance < 750){
+//     console.log("less than 750");
+// }
+// else if(balance < 900){
+//     console.log("less than 900");   
+// }
+// else{
+//     console.log("less than 1200");
+// }
 
 
 const isUserLoggedIn = true;
-if(2==2){
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+if(isUserLoggedIn && debitCard && 2==3){
+    console.log("allowed to buy course");
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
     
 }
